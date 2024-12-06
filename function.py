d55b7e37218d18b19ebf5e57236c8824218cda0f
Hrithwik <hrithwikssukumar@gmail.com>
2024-12-06 19:33:06 +0530

    added function.py

---
+++ function.py
@@ -0,0 +1,38 @@
+
+# function with no return value and no argument
+
+def greet():
+    print("Hello! Welcome to Python programming.")
+
+# Call the function
+greet()
+
+
+# function with arguments and no return value
+
+def display_sum(a, b):
+    print(f"The sum of {a} and {b} is {a + b}.")
+
+# Call the function
+display_sum(5, 3)
+
+
+# Function with No Arguments and a Return Value
+
+def get_pi():
+    return 3.14159
+
+# Call the function
+pi_value = get_pi()
+print(f"Value of Pi: {pi_value}")
+
+
+# Function with Arguments and a Return Value
+def calculate_area(length, width):
+    return length * width
+
+# Call the function
+area = calculate_area(5, 3)
+print(f"The area of the rectangle is {area}.")
+
+
