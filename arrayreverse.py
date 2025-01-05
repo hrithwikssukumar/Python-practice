5c5ef69eb3007771c821ca65230a65c4d282eea9
Hrithwik <hrithwikssukumar@gmail.com>
2025-01-05 20:41:48 +0530

    arrayreverse

---
+++ arrayreverse.py
@@ -0,0 +1,24 @@
+numbers =[12,34,54,32,76,45,86,75,36,29,42]
+n=len(numbers)
+
+for i in range(n-1):
+    for j in range(n-i-1):
+        if numbers[j] > numbers[j+1]:
+            numbers[j],numbers[j+1] = numbers[j+1],numbers[j]
+print(f"The sorted array is :{numbers}")
+
+
+
+
+
+
+#reverse array
+
+numbers =[12,34,54,32,76,45,86,75,36,29,42]
+n=len(numbers)
+
+for i in range(n-1):
+    for j in range(n-i-1):
+        if numbers[j+1] > numbers[j]:
+            numbers[j],numbers[j+1] = numbers[j+1],numbers[j]
+print(f"The sorted array is :{numbers}")
