08a62b24e1c2e66c2c0015d1741b1256eb0d7783
Hrithwik <hrithwikssukumar@gmail.com>
2025-02-06 22:25:18 +0530

    added anagram

---
+++ anagram.py
@@ -0,0 +1,8 @@
+string_1 = input("Enter the first word: ")
+string_2 = input("Enter the second word: ")
+sorted_1 =sorted(string_1)
+sorted_2 =sorted(string_2)
+if sorted_1==sorted_2:
+    print("Words are anagram")
+else:
+    print("Words are not anagram")
